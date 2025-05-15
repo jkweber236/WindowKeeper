7a3a971247b245f96b203be19d06a51f9e674f0d
Jennifer <coo21040@byui.edu>
2025-05-14 23:09:32 -0600

    Altered project name

--- popup.js
+++ popup.js
@@ -2,21 +2,21 @@ chrome.tabs.query({ currentWindow: true }, function(tabs) {
    console.log(tabs)
 });
 
-let saveForm = document.querySelector('#save-workspace-form')
-let saveButton = document.querySelector('.save-workspace-button')
+let saveForm = document.querySelector('#save-window-form')
+let saveButton = document.querySelector('.save-window-button')
 
 saveForm.addEventListener("submit", function(event) {
    event.preventDefault()
 
-   saveWorkspace()
+   saveWindow()
 })
 
-function saveWorkspace() {
-   var workspaceName = document.getElementById("name-input").value;
+function saveWindow() {
+   var windowName = document.getElementById("name-input").value;
 
    chrome.tabs.query({ currentWindow: true }, function(tabs) {
       let currentWindowUrls = tabs.map(tab => tab.url);
-      chrome.storage.local.set({ [workspaceName]: currentWindowUrls }, function() {
+      chrome.storage.local.set({ [windowName]: currentWindowUrls }, function() {
             chrome.storage.local.get(null, function(result) {
                console.log(result);
             });
