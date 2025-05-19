59b38bb26cd6102da432f45b025248ec05fba3fb
Jennifer <coo21040@byui.edu>
2025-05-19 16:03:27 -0600

    Added functionality to extension

--- popup.js
+++ popup.js
@@ -1,13 +1,9 @@
-chrome.tabs.query({ currentWindow: true }, function(tabs) {
-   console.log(tabs)
-});
-
 let saveForm = document.querySelector('#save-window-form')
 let saveButton = document.querySelector('.save-window-button')
+displayWindows();
 
 saveForm.addEventListener("submit", function(event) {
    event.preventDefault()
-
    saveWindow()
 })
 
@@ -17,10 +13,28 @@ function saveWindow() {
    chrome.tabs.query({ currentWindow: true }, function(tabs) {
       let currentWindowUrls = tabs.map(tab => tab.url);
       chrome.storage.local.set({ [windowName]: currentWindowUrls }, function() {
-            chrome.storage.local.get(null, function(result) {
-               console.log(result);
-            });
-      })
+         displayWindows();
+      });
    });
 }
 
+function displayWindows() {
+   windowsList = document.querySelector('.saved-windows')
+   windowsList.innerHTML = "";
+   chrome.storage.local.get(null, function(result) {
+      for (let windowName in result) {
+         const name = document.createElement("h3");
+         name.textContent = windowName;
+         const urlsList = document.createElement("ul")
+
+         for (let url of result[windowName]) {
+            const listItem = document.createElement("li");
+            listItem.textContent = url;
+            urlsList.appendChild(listItem);
+         }
+      
+         windowsList.appendChild(name);
+         windowsList.appendChild(urlsList);
+      }
+   })
+}
